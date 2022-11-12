e21781e6e10edbb5e038ee3c0b38725bbe97c791
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-12 23:06:41 +0900

    [Update] エラーメッセージの日本語化

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -33,4 +33,16 @@ class ClientRequest extends FormRequest
             'registered_at' => 'date|nullable',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前を入力してください',
+            'role.required' => '役職を入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスの形式で入力してください',
+            'age.numeric' => '年齢は数値で入力してください',
+            'registered_at.date' => '日付の形式で入力してください'
+        ];
+    }
 }
