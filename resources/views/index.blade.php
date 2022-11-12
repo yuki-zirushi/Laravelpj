bf0b86809c7447585f203937a3329c74e3db9b9f
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-12 22:22:10 +0900

    [Add] エラーメッセージ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,14 @@
 </head>
 
 <body>
-  <h1>{{$content}}</h1>
+  <p>{{$txt}}</p>
+  @if (count($errors) > 0)
+  <ul>
+    @foreach ($errors->all() as $error)
+    <li>{{$error}}</li>
+    @endforeach
+  </ul>
+  @endif
   <form action="/" method="POST">
     @csrf
     <input type="text" name="content">
