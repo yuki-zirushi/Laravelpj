7293ee095848e7d33cf004c7be8fc9c4bd620677
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-12 22:15:35 +0900

    [Fix] アクションの修正

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,23 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ClientRequest;
 
 class TestController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $data = [
-            'content' => '自由に入力してください',
-        ];
-        return view('index', $data);
+        return view('index', ['txt' => 'フォームを入力']);
     }
 
     public function post(Request $request)
     {
-        $content = $request->content;
-        $data = [
-            'content' => $content . 'と入力しましたね'
-        ];
-        return view('index', $data);
+        return view('index', ['txt' => '正しい入力です']);
     }
 }
