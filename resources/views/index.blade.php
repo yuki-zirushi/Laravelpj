60c7660a555ca674404f394c8f8d4ffefe16e16e
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-12 22:48:34 +0900

    [Update] エラーメッセージの個別化

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,16 +11,63 @@
 <body>
   <p>{{$txt}}</p>
   @if (count($errors) > 0)
-  <ul>
-    @foreach ($errors->all() as $error)
-    <li>{{$error}}</li>
-    @endforeach
-  </ul>
+  <p>入力に問題があります</p>
   @endif
   <form action="/" method="POST">
-    @csrf
-    <input type="text" name="content">
-    <input type="submit">
+    <table>
+      @csrf
+      @if ($errors->has('name'))
+      <tr>
+        <th>ERROR</th>
+        <td>{{$errors->first('name')}}</td>
+      </tr>
+      @endif
+      <tr>
+        <th>名前</th>
+        <td><input type="text" name="name"></td>
+      </tr>
+      @if ($errors->has('role'))
+      <tr>
+        <th>ERROR</th>
+        <td>{{$errors->first('role')}}</td>
+      </tr>
+      @endif
+      <tr>
+        <th>役職</th>
+        <td><input type="text" name="role"></td>
+      </tr>
+      @if ($errors->has('email'))
+      <tr>
+        <th>ERROR</th>
+        <td>{{$errors->first('email')}}</td>
+      </tr>
+      @endif
+      <tr>
+        <th>メール</th>
+        <td><input type="email" name="email"></td>
+      </tr>
+      @if ($errors->has('age'))
+      <tr>
+        <th>ERROR</th>
+        <td>{{$errors->first('age')}}</td>
+      </tr>
+      @endif
+      <tr>
+        <th>年齢</th>
+        <td><input type="text" name="age"></td>
+      </tr>
+      @if ($errors->has('registered_at'))
+      <tr>
+        <th>ERROR</th>
+        <td>{{$errors->first('registered_at')}}</td>
+      </tr>
+      @endif
+      <tr>
+        <th>登録日</th>
+        <td><input type="text" name="registered_at"></td>
+      </tr>
+    </table>
+    <input type="submit" value="送信">
   </form>
 </body>
 
